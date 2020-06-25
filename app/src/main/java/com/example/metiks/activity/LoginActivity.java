8c823d923c7cf1367fee007e2fcf2c14aff42050
igoriag <37387850+igoriag@users.noreply.github.com>
2020-06-25 06:58:56 -0300

    activity_cadastro.xmlc and LoginActivity concluida

--- app/src/main/java/com/example/metiks/activity/LoginActivity.java
+++ app/src/main/java/com/example/metiks/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.metiks.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.metiks.R;
 
@@ -12,5 +14,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirCadastro(View view){
+        Intent i = new Intent(LoginActivity.this , CadastroActivity.class);
+        startActivity(i);
+
     }
 }
