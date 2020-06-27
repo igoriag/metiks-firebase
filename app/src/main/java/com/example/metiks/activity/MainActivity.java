13c3440cfc5a3a6671447371c889ad4bc931d129
igoriag <37387850+igoriag@users.noreply.github.com>
2020-06-27 13:06:20 -0300

    toobar finish

--- app/src/main/java/com/example/metiks/activity/MainActivity.java
+++ app/src/main/java/com/example/metiks/activity/MainActivity.java
@@ -1,17 +1,64 @@
 package com.example.metiks.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.metiks.R;
+import com.example.metiks.helper.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //configura Toolbar
+        Toolbar toobToolbar = findViewById(R.id.toolbarPrincipal);
+        toobToolbar.setTitle("Metiks");
+        setSupportActionBar(toobToolbar);
+
+        //configurar objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
     }
-    //teste cimiit
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_sair:
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try {
+            autenticacao.signOut();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
